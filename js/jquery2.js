9689557447d5395a5c95161f42019621969bb8db
Benjamin Conant <conantbenjamin@gmail.com>
2014-11-11 10:41:18 -0500

    some stuff

--- js/jquery2.js
+++ js/jquery2.js
@@ -4,10 +4,12 @@ $(document).ready(function(){
         NYTapiKey = "bdcebec4874a5076dbaaa7f2a5f0db3b:3:70140904",
         NYTtimesShown = 0,
         NYTresultsArray = [],
+        
         guardianApiKey = "caxj4qkju6y44wsf93aqwkwc",
         guardianTimesShown = 1,
-        guardianResultsArray = [];
-        
+        guardianResultsArray = [],
+        wikipediaResultsArray =[];
+
 
 
 
@@ -50,14 +52,13 @@ $(document).ready(function(){
 
     var apiCallGaurdain = function(searchTerm, resultPageNumber, guardianApiKey) {        
         $.ajax({
-          //  "http://content.guardianapis.com/search?api-key=test&show-fields=all&page=1&q=hello%20kitty"
             url: "http://content.guardianapis.com/search?api-key="+guardianApiKey+"&show-fields=all&page="+ resultPageNumber + "&q="+searchTerm.replace(' ', '%'),
             dataType: 'jsonp',
             success: function( results ) {
             var results = results;
-            console.log(results);
+            //console.log(results);
             var documentsArray = results['response']['results'];
-            console.log(documentsArray);
+            //console.log(documentsArray);
 
             documentsArray.forEach(function(element, index, array){
                     if (element['webUrl'] == null || element['webTitle'] == null || element['fields']['standfirst'] == null) {
@@ -79,21 +80,56 @@ $(document).ready(function(){
             } 
         }
     };
+
+
+    var apiCallWikipedia = function(searchTerm, resultPageNumber, guardianApiKey){
+        $.ajax({
+            url: "http://en.wikipedia.org/w/api.php?format=json&action=query&generator=search&gsrnamespace=0&gsrsearch=" +searchTerm.replace(' ', '+') +"&gsrlimit=10&prop=pageimages|extracts&pilimit=max&exintro&explaintext&exsentences=1&exlimit=max",
+            dataType: 'jsonp',
+            success: function(results) {
+            var results = results;
+            console.log(results);
+            var documentsArray = results['query']['pages'];
+            console.log(documentsArray);
+            console.log(typeof documentsArray);
+
+            for (var prop in documentsArray) {
+                if (documentsArray.hasOwnProperty(prop)){
+                    article = new ResultsObject( "https://en.wikipedia.org/wiki/"+ encodeURI(documentsArray[prop]['title']), documentsArray[prop]['title'], documentsArray[prop]['extract'],  wikipediaResultsArray.length)
+                    wikipediaResultsArray.push(article);
+                }
+            }
+                resultsPrintWikipedia(wikipediaResultsArray);
+                 console.log(wikipediaResultsArray);
+            }
+        });
+    }
+
+    var resultsPrintWikipedia = function(resultsArray){
+        for (var i = 0; i < resultsArray.length; i++){
+            if (resultsArray[i] !== undefined) {
+
+                $("<tr id='wikipeidaResult"+resultsArray[i].index+"'><td class='text-left wikipediaResult'><a href='" + resultsArray[i].url + "'>" + resultsArray[i].headline +"</a><p>" + resultsArray[i].leadParagraph + "</p></td></tr>").insertBefore('#wikipedia-last-row');
+            } 
+        }
+    };
     
     
     $('#search-bar').bind('keypress',function (event){
         if (event.keyCode === 13){
             event.preventDefault();
+            
             userSearchTerm = $('#user-search').val();
+            
             NYTtimesShown = 0;
             NYTresultsArray = [];
             $('.NYTresult').remove();
-            
             
             guardianTimesShown =1;
             guardianResultsArray = [];
             $('.guardianResult').remove();
 
+            apiCallWikipedia(userSearchTerm);
             apiCallNYT(userSearchTerm,NYTtimesShown,NYTapiKey);
             apiCallGaurdain(userSearchTerm, guardianTimesShown, guardianApiKey);
         }
@@ -112,4 +148,30 @@ $(document).ready(function(){
     });
 
 });
+
+
+
+// $.when(ajax1(), ajax2(), ajax3(), ajax4()).done(function(a1, a2, a3, a4){
+//     // the code here will be executed when all four ajax requests resolve.
+//     // a1, a2, a3 and a4 are lists of length 3 containing the response text,
+//     // status, and jqXHR object for each of the four ajax calls respectively.
+// });
+
+// $.when(ajax1(), ajax2(), ajax3(), ajax4()).done(function(a1, a2, a3, a4){
+//     // the code here will be executed when all four ajax requests resolve.
+//     // a1, a2, a3 and a4 are lists of length 3 containing the response text,
+//     // status, and jqXHR object for each of the four ajax calls respectively.
+// });
+
+// function ajax1() {
+//     // NOTE:  This function must return the value 
+//     //        from calling the $.ajax() method.
+//     return $.ajax({
+//         url: "someUrl",
+//         dataType: "json",
+//         data:  yourJsonData,            
+//         ...
+//     });
+// }
+
 
