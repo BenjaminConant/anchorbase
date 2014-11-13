4e2ff379c2efdbcf1939cef53aa3b46472234934
Benjamin Conant <conantbenjamin@gmail.com>
2014-11-13 17:15:52 -0500

    project done

--- js/jquery.js
+++ js/jquery.js
@@ -1,107 +1,169 @@
-function Game() {
-	this.randomNumber = Math.floor((Math.random()*100)+1);
-	this.numberOfGuessess = 10;
-	this.guessArray = [];
-	this.coldHotArray = [];
-	this.hint = function(){
-		$('#output').text(this.randomNumber);
-	}
-	this.playAgain = function(){
-		location.reload(true);
-	}
-
-	this.submitGuess = function(){
-		var userGuessString = $("#user-guess").val();
-		
-		if (isNaN(userGuessString)) {
-			$('#output').text("Please enter a whole number between 1-100!");
-		} else if (userGuessString.indexOf('.') > -1) {
-			$('#output').text("Please enter a whole  number between 1-100!");
-		} else if (parseInt(userGuessString) > 100 || parseInt(userGuessString) < 1) {
-			$('#output').text("make sure your number is between 1-100!");
-		} else {
-			this.numberOfGuessess -= 1;
-			var userGuess = parseInt($("#user-guess").val());
-			if (userGuess === this.randomNumber) {
-				$('#output').text("");
-				$('#guess-left').text("Yay! you win!");	
-				$('#guess-left').css("font-size", "200px");
-			} else if (this.numberOfGuessess === 0) {
-				$('#output').text("");
-				$('#guess-left').text("Sorry, you lose!");
-				$('#guess-left').css("font-size", "200px");
-			} else if (this.guessArray.indexOf(userGuess) != -1) {
-				$('#output').text("You have already guessed " + userGuessString +" try a diffrent number!" )
-			} else {
-				var guessHelperString = "";
-				$('#guessess-left').text(this.numberOfGuessess);
-				
-				this.guessArray.push(userGuess);
-				if (this.guessArray.length === 1 ) {
-					this.coldHotArray.push(userGuess);
-				} else if (Math.abs(this.coldHotArray[this.coldHotArray.length -1] - this.randomNumber) >= Math.abs(userGuess - this.randomNumber)) {
-					this.coldHotArray.push(userGuess);
-				} else {
-					this.coldHotArray.unshift(userGuess)
-				}
-				
-
-
-
-				if ( Math.abs(userGuess - this.randomNumber) > 30) {
-					guessHelperString += "You are cold";
-				} else {
-					guessHelperString += "You are hot";
-				}
-
-				if (this.guessArray.length > 1 && (Math.abs(this.guessArray[this.guessArray.length - 2] - this.randomNumber)) > (Math.abs(userGuess - this.randomNumber))) {
-					guessHelperString += " and getting hotter";
-				} else if (this.guessArray.length > 1) {
-					guessHelperString += " and getting colder";
-				}
-
-				if (userGuess < this.randomNumber) {
-					guessHelperString += ", guess higher!";
-				} else {
-					guessHelperString += ", guess lower!";
-				}
-				$('#user-guess').val('');
-				$('#output').text(guessHelperString);
-				$('#guess-list-span').text(this.coldHotArray.toString());
-			}
-		}
-	}
-}
-
-
-// print answer to the screen
 $(document).ready(function(){	
-	Game = new Game();
 	
-	$('#submit-guess').click(function(){
-		Game.submitGuess();
-	});
+    var userSearchTerm = "",
+        
+        NYTapiKey = "bdcebec4874a5076dbaaa7f2a5f0db3b:3:70140904",
+        NYTtimesShown = 0,
+        NYTresultsArray = [],
+        
+        guardianApiKey = "caxj4qkju6y44wsf93aqwkwc",
+        guardianTimesShown = 1,
+        guardianResultsArray = [],
+        wikipediaResultsArray =[],
 
-	
-	$('#guess-form').submit(function(e){
-		Game.submitGuess();
-		e.preventDefault();
-	})
-
-
-	$('#play-again').click(function(){
-		Game.playAgain();
-	});
-
-	$('#hint').click(function(){
-		Game.hint();
-	});
+        wikipediaContinueString = "";
 
 
 
 
-	
+    function ResultsObject(url, headline, leadParagraph, index) {
+        this.url = url;
+        this.headline = headline;
+        this.leadParagraph = leadParagraph;
+        this.index = index;
+    }
+
+    var apiCallNYT = function(){
+        $.ajax({
+            url: "http://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + userSearchTerm.replace(' ', '+') + "&page=" + NYTtimesShown + "&api-key=" + NYTapiKey,  
+            dataType: 'json',
+            success: function(results){
+                var resultsString = JSON.stringify(results),
+                    resultsObject = results,
+                    documentsArray = resultsObject['response']['docs'];
+               
+                documentsArray.forEach(function(element, index, array){
+                    if (element['web_url'] == null || element['headline']['main'] == null || element['lead_paragraph'] == null) {
+                    } else {
+                    article = new ResultsObject(element['web_url'], element['headline']['main'], element['lead_paragraph'],  NYTresultsArray.length);
+                    NYTresultsArray.push(article);
+                    }
+                });
+                resultsPrintNYT(NYTresultsArray);
+                NYTtimesShown++;
+            }
+        });
+    };
+    
+    var resultsPrintNYT = function(resultsArray){
+        for (var i = 0; i < resultsArray.length; i++){
+            if (resultsArray[i] !== undefined) {
+                $("<tr id='NYTresult"+resultsArray[i].index+"'><td class='text-left NYTresult'><a href='" + resultsArray[i].url + "'>" + resultsArray[i].headline +"</a><p>" + resultsArray[i].leadParagraph + "</p></td></tr>").insertBefore('#NYT-last-row');
+            } 
+        }
+    };
+
+    var apiCallGaurdain = function() {        
+        $.ajax({
+            url: "http://content.guardianapis.com/search?api-key="+guardianApiKey+"&show-fields=all&page="+ guardianTimesShown + "&q="+userSearchTerm.replace(' ', '%'),
+            dataType: 'jsonp',
+            success: function( results ) {
+            var results = results;
+            //console.log(results);
+            var documentsArray = results['response']['results'];
+            //console.log(documentsArray);
+
+            documentsArray.forEach(function(element, index, array){
+                    if (element['webUrl'] == null || element['webTitle'] == null || element['fields']['standfirst'] == null) {
+                    } else {
+                    article = new ResultsObject(element['webUrl'], element['webTitle'], element['fields']['standfirst'],  guardianResultsArray.length);
+                    guardianResultsArray.push(article);
+                    }
+                });
+                resultsPrintGuardian(guardianResultsArray);
+                guardianTimesShown++;
+            }
+        });
+    };
+
+    var resultsPrintGuardian = function(resultsArray){
+        for (var i = 0; i < resultsArray.length; i++){
+            if (resultsArray[i] !== undefined) {
+                $("<tr id='guardianResult"+resultsArray[i].index+"'><td class='text-left guardianResult'><a href='" + resultsArray[i].url + "'>" + resultsArray[i].headline +"</a><p>" + resultsArray[i].leadParagraph + "</p></td></tr>").insertBefore('#guardian-last-row');
+            } 
+        }
+    };
+
+
+    var apiCallWikipedia = function(){
+        $.ajax({
+            //"http://en.wikipedia.org/w/api.php?format=json&action=query&generator=search&gsrnamespace=0&gsrsearch=" +searchTerm.replace(' ', '+') +"&gsrlimit=10&prop=pageimages|extracts&pilimit=max&exintro&explaintext&exsentences=1&exlimit=max&continue=" + wikipediaContinueString
+            url: "http://en.wikipedia.org/w/api.php?format=json&action=query&generator=allpages&gaplimit=10&gapfrom="+ userSearchTerm.replace(' ', '+')+"&prop=pageimages|extracts&pilimit=max&exintro&explaintext&exsentences=1&exlimit=max&gapcontinue=" + wikipediaContinueString,
+            dataType: 'jsonp',
+            success: function(results) {
+            var results = results;
+            console.log(results);
+            var documentsArray = results['query']['pages'];
+            console.log(documentsArray);
+            console.log(typeof documentsArray);
+            wikipediaContinueString = results['query-continue']['allpages']['gapcontinue'];
+
+            for (var prop in documentsArray) {
+                if (documentsArray.hasOwnProperty(prop)){
+                    article = new ResultsObject( "https://en.wikipedia.org/wiki/"+ encodeURI(documentsArray[prop]['title']), documentsArray[prop]['title'], documentsArray[prop]['extract'],  wikipediaResultsArray.length)
+                    wikipediaResultsArray.push(article);
+                }
+            }
+                 resultsPrintWikipedia(wikipediaResultsArray);
+                 console.log(wikipediaResultsArray);
+            }
+        });
+    }
+
+    var resultsPrintWikipedia = function(resultsArray){
+        for (var i = 0; i < resultsArray.length; i++){
+            if (resultsArray[i] !== undefined) {
+
+                $("<tr id='wikipeidaResult"+resultsArray[i].index+"'><td class='text-left wikipediaResult'><a href='" + resultsArray[i].url + "'>" + resultsArray[i].headline +"</a><p>" + resultsArray[i].leadParagraph + "</p></td></tr>").insertBefore('#wikipedia-last-row');
+            } 
+        }
+    };
+   
+    $('#search-bar').bind('keypress',function (event){
+        if (event.keyCode === 13){
+            event.preventDefault();
+            
+            userSearchTerm = $('#user-search').val();
+            
+            NYTtimesShown = 0;
+            NYTresultsArray = [];
+            $('.NYTresult').remove();
+            
+            guardianTimesShown =1;
+            guardianResultsArray = [];
+            $('.guardianResult').remove();
+
+            wikipediaContinueString = "";
+            wikipediaResultsArray =[];
+            $('.wikipediaResult').remove();
+
+
+            apiCallWikipedia();
+            apiCallNYT();
+            apiCallGaurdain();
+        }
+    });
+
+    $('#NYT-results-table-scrolable-container').bind('scroll', function() {
+        if($(this).scrollTop() + $(this).innerHeight() >= this.scrollHeight) {
+            guardianResultsArray = [];
+            apiCallNYT();
+        }
+    });
+
+    $('#guardian-results-table-scrolable-container').bind('scroll', function() {
+        if($(this).scrollTop() + $(this).innerHeight() >= this.scrollHeight) {
+            guardianResultsArray =[];
+            apiCallGaurdain();
+        }
+    });
+
+    $('#wikipedia-results-table-scrolable-container').bind('scroll', function() {
+        if($(this).scrollTop() + $(this).innerHeight() >= this.scrollHeight) {
+            wikipediaResultsArray =[];
+            apiCallWikipedia();
+        }
+    });
+
 });
-
-
 
